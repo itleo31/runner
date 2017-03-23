73aa831e0b92f272a0139e35bc72a424bcbaddcd
Khanh Pham <khanhpham@Fossil.com>
2017-03-23 13:29:44 +0700

    Initial Commit

---
+++ Runner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Runner
+//
+//  Created by Khanh Pham on 3/23/17.
+//  Copyright © 2017 Khanh Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
